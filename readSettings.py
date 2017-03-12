e3f264dfc3a2e8cf602ef78c52b0f1242adc4542
Hantzley Tauckoor <hantzley@gmail.com>
2017-03-12 14:10:08 +1100

    Modified readSettings.py script

--- readSettings.py
+++ readSettings.py
@@ -3,5 +3,9 @@ with open('settings.txt','r') as f:
     settings = f.readlines()
 f.close()
 firstSetting = settings[0].rstrip()
+secondSetting = settings[1].rstrip()
 
 print(firstSetting)
+print(secondSetting)
+
+
