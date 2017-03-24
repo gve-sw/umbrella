40fc3303ba86ff9afc65c613dca52e8908ee28cb
abhr <abhr@cisco.com>
2017-03-24 14:40:01 +0530

    Abhijith

---
+++ step 1/emailWhois.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import readSettings
+from urllib2 import Request, urlopen
+import os, sys
+import json
+import pprint
+
+#Fetch API token from setting.txt
+def getToken():
+	appSettings = readSettings.loadSettings("../../settings.txt")
+
+	firstSetting = appSettings[0].rstrip()
+	return firstSetting
+token=getToken()
+
+#Validate token
+if not token:
+  print "ERROR: environment variable \'INVESTIGATE_TOKEN\' not set. Invoke script with \'INVESTIGATE_TOKEN=%YourToken% python scripts.py\'"
+  sys.exit(1)
+
+#Reading and validating commnad line argument
+try:
+    email=sys.argv[1]
+except IndexError:
+    print "Please enter a email address\nUsage: emailWhois.py <emailID> (Eg: file.py test@example.com)"
+    sys.exit(1)
+
+# domains/categorization
+
+headers = {
+  'Authorization': 'Bearer ' + token
+}
+#This API method returns the WHOIS information for the specified email address
+request = Request('https://investigate.api.umbrella.com/whois/emails/'+email, headers=headers)
+
+response_body = urlopen(request).read()
+print response_body
