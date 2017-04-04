de73498d878cdde1fd83604272142a40d6152f58
repillai <repillai@cisco.com>
2017-04-04 10:42:30 +0530

    Renjana
    5 new methods added

---
+++ step 1/latestDomains.py
@@ -0,0 +1,33 @@
+import readSettings
+from urllib2 import Request, urlopen
+import os, sys
+import json
+import pprint
+
+#Fetch API token from setting.txt
+def getToken():
+	appSettings = readSettings.loadSettings("../../settings.txt")
+
+	firstSetting = appSettings[0].rstrip()
+	return firstSetting
+token=getToken()
+
+#Validate token
+if not token:
+  print "Token not set"
+  sys.exit(1)
+
+#Reading and validating commnad line argument
+try:
+    domainName=sys.argv[1]
+except IndexError:
+    print "Please enter a domain name\nUsage: domainShowLabels.py <Domain Name> (Eg: file.py example.com)"
+    sys.exit(1)
+# latest_domains
+
+headers = {
+  'Authorization': 'Bearer ' + token
+}
+request = Request('https://investigate.api.opendns.com/ips/'+domainName+'.json', headers=headers)
+response_body = urlopen(request).read()
+print "latest_domains: " + response_body
---
+++ step 1/linksName.py
@@ -0,0 +1,33 @@
+import readSettings
+from urllib2 import Request, urlopen
+import os, sys
+import json
+import pprint
+
+#Fetch API token from setting.txt
+def getToken():
+	appSettings = readSettings.loadSettings("../../settings.txt")
+
+	firstSetting = appSettings[0].rstrip()
+	return firstSetting
+token=getToken()
+
+#Validate token
+if not token:
+  print "Token not set"
+  sys.exit(1)
+
+#Reading and validating commnad line argument
+try:
+    domainName=sys.argv[1]
+except IndexError:
+    print "Please enter a domain name\nUsage: domainShowLabels.py <Domain Name> (Eg: file.py example.com)"
+    sys.exit(1)
+# links/name
+
+headers = {
+  'Authorization': 'Bearer ' + token
+}
+request = Request('https://investigate.api.opendns.com/links/name/'+domainName+'.json', headers=headers)
+response_body = urlopen(request).read()
+print "links/name: " + response_body
